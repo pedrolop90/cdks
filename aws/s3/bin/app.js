d0d4bb3182ab62b9c9873c68329659156bc7c131
Pedro.Lopez <pedro.lopez@pragma.com.co>
2025-03-29 01:54:38 -0500

    modificado para que no tuviera en cuenta las credenciales

--- aws/s3/bin/app.js
+++ aws/s3/bin/app.js
@@ -1,42 +1,14 @@
 require('dotenv').config();
 
+const cdk = require("aws-cdk-lib");
+const { S3Stack } = require('../lib/s3-stack-aws');
 const { obtenerConfiguracion } = require('./obtener-variables-entorno');
-const { obtenerCredenciales } = require('../obtener-credenciales');
+
 
 async function main() {
     let variables = obtenerConfiguracion();
 
-    console.log("🔄 Obteniendo credenciales...");
-    const credenciales = await obtenerCredenciales(variables);
-
-    // 🔥 Establecer credenciales en el proceso antes de ejecutar CDK
-    process.env.AWS_ACCESS_KEY_ID = credenciales.accessKey;
-    process.env.AWS_SECRET_ACCESS_KEY = credenciales.secretKey;
-
-    console.log("🚀 Credenciales actualizadas para CDK");
-
-    // 🔥 Crear una nueva instancia de credenciales y forzar a CDK a usarlas
-    const aws = require("aws-sdk");
-    aws.config.credentials = new aws.Credentials(credenciales.accessKey, credenciales.secretKey);
-
-    
-    // 🔥 Usar `AWS.CredentialProviderChain` para asegurarse de que CDK tome estas credenciales
-    aws.config.credentialProvider = new aws.CredentialProviderChain([
-        () => new aws.Credentials(credenciales.accessKey, credenciales.secretKey),
-      ]);
-
-    process.env.AWS_SDK_LOAD_CONFIG = "1";
-
-    const cdk = require("aws-cdk-lib");
-    const { S3Stack } = require('../lib/s3-stack-aws');
     const app = new cdk.App();
-
-    variables.env = {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION,
-    };
-
-
     new S3Stack(app, variables.idStack, variables);
 
     app.synth();
